4a5415917e15a95020821783aa8eb4691b7a08ef
ST0817 <136716138+ST0817@users.noreply.github.com>
2026-05-02 20:10:40 +0900

    added REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use rustyline::{DefaultEditor, error::ReadlineError};
+
+fn main() -> ExitCode {
+    let mut editor = DefaultEditor::new().unwrap();
+
+    loop {
+        match editor.readline("❯ ") {
+            Ok(src) if !src.is_empty() => {
+                println!("Got: {src}");
+            }
+            Ok(_) => {},
+            Err(ReadlineError::Eof) => break ExitCode::SUCCESS,
+            Err(error) => {
+                eprintln!("{error}");
+                break ExitCode::FAILURE;
+            }
+        }
+    }
+
 }
